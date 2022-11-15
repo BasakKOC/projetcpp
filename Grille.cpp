f0b837fda59d36fc0a2a1a1fa9055820f36c0f05
melx1998 <melissa.mansoor@outlook.com>
2022-11-15 18:19:37 +0100

    Maj 3 15 nov 2022

--- Grille.cpp
+++ Grille.cpp
@@ -11,6 +11,13 @@ using std::vector;
 using std::pair;
 
 //Grille
+//constructeurs
+Grille::Grille() : grid(){
+    vector<int> l(10);
+    vector<vector<int>> k(10,l);
+    grid=k;
+} //par defaut
+
 Grille::Grille(const Grille &g):  grid() { //par copie
         for (int i=0; i<10; i++){
             for(int j=0; j<10; j++){
@@ -30,7 +37,7 @@ std::ostream& operator<<(std::ostream& out, const Grille &g){ //getter et setter
 
 //Grille Depart
 //constructeur
-GrilleDepart::GrilleDepart() : grid(){ //par defaut
+GrilleDepart::GrilleDepart() : Grille(){ //par defaut
             for (int i=0; i<10; i++) {
                 vector<int> ligne;
                 for (int j = 0; j <10; j++) {
@@ -40,7 +47,7 @@ GrilleDepart::GrilleDepart() : grid(){ //par defaut
     }
 
 //GrilleJeu
-GrilleJeu::GrilleJeu() : grid(){ //par defaut
+GrilleJeu::GrilleJeu() : Grille(){ //par defaut
             for (int i=0; i<10; i++) {
                 vector<int> ligne;
                 for (int j = 0; j <10; j++) {
@@ -49,7 +56,6 @@ GrilleJeu::GrilleJeu() : grid(){ //par defaut
                 grid.push_back(ligne);}
     }
 
-
 //methodes
 pair<char, int> Grille::gametocoord(char col, int ligne){
         pair<int, int> res;
@@ -57,7 +63,13 @@ pair<char, int> Grille::gametocoord(char col, int ligne){
         res.second=ligne-1;
         return res;} 
 
-void Grille::placer(Bateau bateau, int startx, int starty, int endx, int endy){
+void Grille::placer(Bateau bateau){
+    //recuperation coordonnees
+    int startx = bateau.x_start(); //getters de la classe Bateau
+    int starty = bateau.y_start();
+    int endx = bateau.x_end();
+    int endy = bateau.y_end();
+
     //tests validite coordonnees
     assert(startx==endx || starty==endy);
     int longueur;
