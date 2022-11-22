8e32ae67dbc5af15c9faae8cb0f6f48d283a7108
melx1998 <36272739+melx1998@users.noreply.github.com>
2022-11-22 15:18:25 +0100

    Add files via upload
    test de placer

--- testgrille.cpp
+++ testgrille.cpp
@@ -9,13 +9,28 @@ using std::endl;
 using std::vector;
 
 int main(){
-	Grille g;
+	/* Grille g;
 	cout<<"g="<<endl<<g<<endl;
 
 	GrilleJeu gjeu;
 	cout<<"gjeu="<<endl<<gjeu<<endl;
 	
 	GrilleDepart gdepart;
-	cout<<"gdepart="<<endl<<gdepart<<endl;
-	
+	cout<<"gdepart="<<endl<<gdepart<<endl; */
+
+	GrilleDepart g;
+	cout<<endl<<"Depart g = "<<endl<<g<<endl;
+	vector<Bateau> bat1 = g.get_bat();
+	cout<< "bat = ";
+	for (Bateau boat: bat1){
+    	cout << boat << ' ';}
+	Bateau b(5, 3, 5, 7, 5); //(startx=3, starty=5); (endx=7, endy=5)
+	cout<<endl<<"taille b = "<<b.size();
+	g.placer(b);
+	cout<<endl<<"New g = "<<endl<<g<<endl;
+	bat1 = g.get_bat();
+	cout<< "New bat = ";
+	for (Bateau boat: bat1){
+    	cout << boat << ' ';}
+
 }
