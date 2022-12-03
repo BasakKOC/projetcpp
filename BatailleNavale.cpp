b98a54e194a0628afb224634fe35df9b3583f5d5
melx1998 <melissa.mansoor@outlook.com>
2022-12-03 18:49:13 +0100

    Ajout methode turn et fonctions tests de coordonnees dans BatailleNavale

--- BatailleNavale.cpp
+++ BatailleNavale.cpp
@@ -23,27 +23,44 @@ BatailleNavale::BatailleNavale(){
 }
 
 
-// Turn of player 1
-void BatailleNavale::turn_1(int x, int y){
-    switch(player2_self(x, y)){
-        case 0: // water
-                cout << "A l'eau !" << endl;
-                player1_rival.actualiser(x, y, player2_self);
-                break;
-        case 1: // nouvelle case bateau
-                cout << "Touche !" << endl;
-                player1_rival.actualiser(x, y, player2_self);
-                break;
-        default: // already touched (2) or sank (3)
-            cout << "Case deja touchee !" << endl;
+//methodes
+bool BatailleNavale::test_coord_tir (const GrilleJeu& player_rival, int x, int y) const{
+    //tests sur les coordonnees :
+    //condgrid : teste coord dans la grille
+    bool condgrid = (x > -1 && x < 10) && (y > -1 && y < 10);
+
+    //conduntouched : teste si c'est une case non touchee
+    bool conduntouched = false;
+    if (condgrid){
+        conduntouched = (player_rival(x, y) == 4);
     }
-    player1_rival.affiche();
+    return condgrid && conduntouched;
 }
 
+bool BatailleNavale::test_coord_prepare(const GrilleDepart &player_self, int start_x, int start_y, int end_x, int end_y, int size_boat) const{
+    //tests sur les coordonnees :
+    //condgrid : teste coord dans la grille
+    bool condgrid = (start_x > -1 && start_x < 10) && (end_x > -1 && end_x < 10) && (start_y > -1 && start_y < 10) && (end_y > -1 && end_y < 10);
 
-// Turn of AI
-void BatailleNavale::turn_2(int x, int y){ // nothing happens for: water (0), already touched (2) or sank (3) - note: AI should not repeat movements
-    player2_rival.actualiser(x, y, player1_self);
+    //condshape : teste la forme du bateau (horizontal ou vertical)
+    bool condshape = start_x == end_x || start_y == end_y;
+
+    //condsize : teste taille bateau
+    int longueur;
+    if(start_x == end_x){longueur = abs(end_y - start_y) +1;}
+    else{longueur = abs(start_x - end_x) +1;}
+    bool condsize = (longueur == size_boat);
+
+    //conduntouched : teste si c'est une case non touchee
+    bool conduntouched = false;
+    if (condgrid && condshape && condsize){
+        for (int i=start_x; i<=end_x; i++){
+            for(int j=start_y; j<=end_y; j++){ 
+                conduntouched = (player_self(i, j) == 0);
+            }
+        }
+    }
+    return condgrid && condshape && condsize && conduntouched;
 }
 
 
@@ -60,27 +77,13 @@ void BatailleNavale::prepare_game(){ // 2 - IDEA: CREATE AUX METHOD FOR CHECKING
             cout << "Respect this order: start_x, start_y, end_x, end_y." << endl;
             cin >> start_x;
             cin >> start_y; 
-            cin >> end_x; 
+            cin >> end_x;
             cin >> end_y;
-            bool cond1 = (start_x > -1 && start_x < 10) && (end_x > -1 && end_x < 10) && (start_y > -1 && start_y < 10) && (end_y > -1 && end_y < 10);
-            bool cond2 = start_x == end_x || start_y == end_y;
-            int longueur;
-            if(start_x == end_x){longueur = abs(end_y - start_y) +1;}
-            else{longueur = abs(start_x - end_x) +1;}
-            bool cond3 = longueur == sizes[i];
-            bool cond4;
-            for (int i=start_x; i<=end_x; i++){
-                for(int j=start_y; j<=end_y; j++){ 
-                    cond4 = player1_self(i, j) == 0;
-                }
-            }
-            
-            if(cond1 == true && cond2 == true && cond3 == true && cond4 == true){
-                break;
-            }
+            if(test_coord_prepare(player1_self, start_x, start_y, end_x, end_y, sizes[i])){
+                break;}
         }
-        Bateau b1(sizes[i], start_x, start_y, end_x, end_y);
-        player1_self.placer(b1);
+        Bateau b(sizes[i], start_x, start_y, end_x, end_y);
+        player1_self.placer(b);
         cout << player1_self;
         player2_rival.recupere_bat(player1_self);
     }
@@ -91,42 +94,39 @@ void BatailleNavale::prepare_game(){ // 2 - IDEA: CREATE AUX METHOD FOR CHECKING
     player2_self = ia.preparer_IA();
     player1_rival.recupere_bat(player2_self);
     cout<<"grille IA :"<<endl<<player2_self<<endl;
-    
-    // 1 - TEST this
-    //player2_self = player1_self;
-    
-/*
-    // Setting the ships of AI (random) -- NOT WORKING
-    for (int i = 0; i < sizeof(sizes) / sizeof(int); i++) {
-        while(true){
-            srand (time(NULL));
-            start_x = rand() % 9;
-            start_y = rand() % 9;
-            end_x = rand() % 9;
-            end_y = rand() % 9;
 
-            bool cond1 = (start_x > -1 && start_x < 10) && (end_x > -1 && end_x < 10) && (start_y > -1 && start_y < 10) && (end_y > -1 && end_y < 10);
-            bool cond2 = start_x == end_x || start_y == end_y;
-            int longueur;
-            if(start_x == end_x){longueur = abs(end_y - start_y) +1;}
-            else{longueur = abs(start_x - end_x) +1;}
-            bool cond3 = longueur == sizes[i];
-            
-            if(cond1 == true && cond2 == true && cond3 == true){
+}
+
+// Turn of player 1
+void BatailleNavale::turn(int x, int y, const GrilleDepart &playeri_self, GrilleJeu &playerj_rival){
+    switch(playeri_self(x, y)){
+        case 0: // water
+                cout << "A l'eau !" << endl;
+                playerj_rival.actualiser(x, y, playeri_self);
                 break;
-            }
+        case 1: // nouvelle case bateau
+                cout << "Touche !" << endl;
+                playerj_rival.actualiser(x, y, playeri_self);
+                break;
+        default: // already touched (2) or sank (3)
+            cout << "Case deja touchee !" << endl;
         }
-        Bateau b2(sizes[i], start_x, start_y, end_x, end_y);
-        player2_self.placer(b2);
-        cout << player2_self;
-    }*/
 }
 
 
-Grille BatailleNavale::jouer(){
-    prepare_game();
-
-    while(true){
+void BatailleNavale::jouer(){
+    //prepare_game();
+    IA ia;
+    
+    player2_self = ia.preparer_IA();
+    cout<<"player2_self ="<<endl<<player2_self<<endl;
+    player1_rival.recupere_bat(player2_self);
+    cout<<"player1_rival ="<<endl<<player1_rival<<endl;
+    pair<int, int> p = ia.level_random(player1_rival);
+    turn(p.first, p.second,player2_self,player1_rival);
+    cout<<"player1_rival ="<<endl<<player1_rival<<endl;
+    //il faut tout changer! bisous
+    /* while(true){
          // Turn of player 1
         cout << "TURN OF PLAYER 1" << endl;
         //cout << player1_rival << endl;
@@ -157,6 +157,7 @@ Grille BatailleNavale::jouer(){
             cout << "YOU LOSE!" << endl;
             break; 
         }
-    }
+    } */
+    
 }
 
--- test_bataille.cpp
+++ test_bataille.cpp
@@ -1,6 +1,7 @@
 #include "Grille.hpp"
 #include "Bateau.hpp"
 #include "BatailleNavale.hpp"
+#include "IA.hpp"
 #include <iostream>
 #include <cassert>
 #include <vector>
@@ -12,16 +13,34 @@ using std::vector;
 int main(){
     BatailleNavale bataille;
     int i,j;
-    bataille.prepare_game();
-    for(int j=0; j<=100;j++){
+
+    // Test prepare_game
+    /* bataille.prepare_game(); */
+
+    // Test turn_1
+    /* for(int j=0; j<=100;j++){
         cout<<"ligne ?"<<endl;
         cin>>i;
         cout<<"colonne ?"<<endl;
         cin>>j;
         bataille.turn_1(i,j);
-    }
+    } */
+
+    // Test turn_2
+    /* IA ia;
     
+    GrilleDepart gd_ia = ia.preparer_IA();
+    GrilleJeu gj_ia;
+    gj_ia.recupere_bat(gd_ia);
+    cout<<"gj_ia ="<<endl<<gj_ia<<endl;
+    pair<int, int> p = ia.level_random(gj_ia);
+    bataille.turn_2(p.first, p.second);
+    gj_ia.actualiser(x, y, gj); */
+
+    bataille.prepare_game();
     //bataille.jouer();
+
+
 
     return 0;
 }
